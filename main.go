8f94e5cecd53d062e246d77c42c5c0cc128408f5
parkchoongho <pchyo92@gmail.com>
2020-05-11 15:56:17 +0900

    #1.2 Variables and Constants

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/parkchoongho/learngo/something"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
-	something.SayHello()
+	name := "nico"
+	// 함수 내부에서만 위와 같이 선언이 가능합니다. 또한 첫 번쨰 값을 기준으로 변수의 타입이 결정됩니다.
+	// var name string = "nico"
+	name = "Park"
+	fmt.Println(name)
 }
