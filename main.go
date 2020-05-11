f6f22116f611822ff3a513c97dac10eb18cba453
parkchoongho <pchyo92@gmail.com>
2020-05-11 16:57:57 +0900

    #1.3 Functions Part One

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// 들어오는 argument의 값이 정해져 있지 않은 경우 이렇게 작성할 수 있습니다.
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
+// GO는 특이하게 함수가 동시에 여러개의 값을 반환할 수 있습니다.
+func lenAndUpprt(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+// GO는 argument와 return 값의 type을 함수에 명시해야합니다.
+func multiply(a, b int) int {
+	return a * b
+}
 
 func main() {
-	name := "nico"
-	// 함수 내부에서만 위와 같이 선언이 가능합니다. 또한 첫 번쨰 값을 기준으로 변수의 타입이 결정됩니다.
-	// var name string = "nico"
-	name = "Park"
-	fmt.Println(name)
+	fmt.Println(multiply(2, 2))
+	totalLength, upperName := lenAndUpprt("dd")
+	fmt.Println(totalLength, upperName)
+	repeatMe("DD", "Hi")
 }
