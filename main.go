096db16799cb3eb55f665c46823c7161ccc804c5
parkchoongho <pchyo92@gmail.com>
2020-05-15 13:59:33 +0900

    #1.8 Pointers

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import "fmt"
 
-// 일반적인 switch문과 크게 다르지 않습니다. if문과 마찬가지로 variavle expression을 사용할 수 있습니다.
-func canIDrink(age int) bool {
-	switch koreanAge := age + 2; koreanAge >= 20 {
-	case true:
-		return true
-	case false:
-		return false
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink(17))
+	// a := 2
+	// b := a
+	// a = 10
+	// fmt.Println(a, b)
+
+	// a := 2
+	// b := &a
+	// a = 5
+	// fmt.Println(&a, b)
+	// fmt.Println(a, *b)
+
+	a := 2
+	b := &a
+	*b = 5
+	fmt.Println(a)
 }
