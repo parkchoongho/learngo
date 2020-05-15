ff4554449bc753968ee132268eac3b42b62c14d0
parkchoongho <pchyo92@gmail.com>
2020-05-15 16:03:41 +0900

    #1.11 Structs

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	// map은 자바스크립트의 Object와 유사하지만 다른 자료구조입니다. key,value 형태인데 type이 결정되어 있습니다.
-	nico := map[string]string{"name": "nico", "age": "12"}
-	for _, value := range nico {
-		fmt.Println(value)
-	}
+	// favFood := []string{"kimchi", "ramen"}
+	// nico := person{"박충호", 29, favFood}
+	// fmt.Println(nico)
+
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{name: "박충호", age: 29, favFood: favFood}
+	fmt.Println(nico)
 }
