734143e0cd9e2403db5280f2f0115d0c3a672717
parkchoongho <pchyo92@gmail.com>
2020-05-15 15:33:30 +0900

    #1.10 Maps

--- main.go
+++ main.go
@@ -3,14 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	// array는 크기가 정해져 있는 배열, 배열 크기를 제한하지 않고 싶자면 slice를 사용
-	// names := [5]string{"nico", "lynn", "dal"}
-	// names[3] = "lalala"
-	// fmt.Println(names[4])
-
-	names := []string{"nico", "lynn", "dal"}
-	// names[3] = "lalala"
-	// append는 names을 수정하지 않고 새로운 변수가 추가된 slice를 반환합니다.
-	names = append(names, "lalala")
-	fmt.Println(names)
+	// map은 자바스크립트의 Object와 유사하지만 다른 자료구조입니다. key,value 형태인데 type이 결정되어 있습니다.
+	nico := map[string]string{"name": "nico", "age": "12"}
+	for _, value := range nico {
+		fmt.Println(value)
+	}
 }
