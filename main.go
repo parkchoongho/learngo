887dbcd86cb96f46801496cddaf03e59cbbc8866
parkchoongho <pchyo92@gmail.com>
2020-05-15 15:04:05 +0900

    #1.9 Arrays and Slices

--- main.go
+++ main.go
@@ -3,19 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	// a := 2
-	// b := a
-	// a = 10
-	// fmt.Println(a, b)
+	// array는 크기가 정해져 있는 배열, 배열 크기를 제한하지 않고 싶자면 slice를 사용
+	// names := [5]string{"nico", "lynn", "dal"}
+	// names[3] = "lalala"
+	// fmt.Println(names[4])
 
-	// a := 2
-	// b := &a
-	// a = 5
-	// fmt.Println(&a, b)
-	// fmt.Println(a, *b)
-
-	a := 2
-	b := &a
-	*b = 5
-	fmt.Println(a)
+	names := []string{"nico", "lynn", "dal"}
+	// names[3] = "lalala"
+	// append는 names을 수정하지 않고 새로운 변수가 추가된 slice를 반환합니다.
+	names = append(names, "lalala")
+	fmt.Println(names)
 }
